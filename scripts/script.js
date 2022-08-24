01380102814cf0c4754f187ac47a7e1e1f82b85a
vicious7577 <antony.fyodorov@gmail.com>
2022-08-24 14:59:26 +0300

    get data to card

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,36 @@
 const buttonMen = document.querySelector('.header__button-gender_men');
 const buttonWomen = document.querySelector('.header__button-gender_women');
 const body = document.body
+const cardImage = document.querySelector('.card__image');
+const cardText = document.querySelector('.card__text');
+
 
 const state = {
     gender: body.classList.contains('women') ? 'women' : 'men',
 };
+
+const getRandomForArr = (arr) => {
+    const randomNumber = Math.floor(Math.random() * arr.length);
+    return arr[randomNumber]
+};
+
+const getData = () => fetch('db.json').then((response) => response.json());
+
+const getDataToCard = () => {
+    getData().then(data => {
+        state.text = getRandomForArr(data.text[state.gender]);
+        state.photo = getRandomForArr(data.photo[state.gender]);
+        cardImage.src = `img/${state.photo}`;
+        cardText.innerHTML = state.text;
+    });
+}
 
 const changeToMen = () => {
     if (state.gender !== 'men') {
         body.classList.add('men');
         body.classList.remove('women');
         state.gender = 'men';
+        getDataToCard();
     };
 };
 
@@ -19,8 +39,10 @@ const changeToWomen = () => {
         body.classList.add('women');
         body.classList.remove('men');
         state.gender = 'women';
+        getDataToCard();
     }
 };
 
 buttonMen.addEventListener('click', changeToMen);
 buttonWomen.addEventListener('click', changeToWomen);
+getDataToCard();
