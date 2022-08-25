58849cfd7686240889d53b2e297261097277aeec
vicious7577 <antony.fyodorov@gmail.com>
2022-08-25 14:28:21 +0300

    add html to canvas

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ const buttonWomen = document.querySelector('.header__button-gender_women');
 const body = document.body
 const cardImage = document.querySelector('.card__image');
 const cardText = document.querySelector('.card__text');
+const buttonText = document.querySelector('.header__button-change_text');
+const buttonImage = document.querySelector('.header__button-change_image');
 
 
 const state = {
@@ -16,12 +18,22 @@ const getRandomForArr = (arr) => {
 
 const getData = () => fetch('db.json').then((response) => response.json());
 
+const changeDOM = () => {
+    if (state.photo.includes('black')) {
+        cardText.style.color = '#fff';
+    } else {
+        cardText.style.color = '';
+    };
+
+    cardImage.src = `img/${state.photo}`;
+    cardText.innerHTML = state.text.replaceAll('\n', '<br>');
+};
+
 const getDataToCard = () => {
     getData().then(data => {
         state.text = getRandomForArr(data.text[state.gender]);
         state.photo = getRandomForArr(data.photo[state.gender]);
-        cardImage.src = `img/${state.photo}`;
-        cardText.innerHTML = state.text;
+        changeDOM();
     });
 }
 
@@ -43,6 +55,41 @@ const changeToWomen = () => {
     }
 };
 
+const changeText = () => {
+    getData().then(data => {
+        state.text = getRandomForArr(data.text[state.gender]);
+        changeDOM();
+    });
+};
+
+const changeImage = () => {
+    getData().then(data => {
+        state.photo = getRandomForArr(data.photo[state.gender]);
+        changeDOM();
+    });
+};
+
 buttonMen.addEventListener('click', changeToMen);
 buttonWomen.addEventListener('click', changeToWomen);
+buttonText.addEventListener('click', changeText);
+buttonImage.addEventListener('click', changeImage);
 getDataToCard();
+
+const cardWrapper = document.querySelector('.card__wrapper');
+
+cardWrapper.addEventListener('dblclick', () => {
+
+    const newWindow = window.open(
+        '',
+        '',
+        `width=840,height=520,
+        top=${(screen.height / 2) - 520 / 2},
+        left=${(screen.width / 2) - 840 / 2}`
+        );
+
+    html2canvas(cardWrapper).then(canvas => {
+        canvas.style.maxWidth = '100%';
+        canvas.style.height = 'auto';
+        newWindow.document.body.append(canvas);
+    });
+});
